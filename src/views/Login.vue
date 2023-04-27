e48515c3afae185ed3c779829526f69536d7243a
MyMatrixs <wangwanli438@gmail.com>
2023-04-27 21:10:55 +0800

    尝试使用elementUI，还很迷糊

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,13 +1,22 @@
 <template>
-  <div class="main">{{mydata}}</div>
+  <div>
+    <el-container>
+    <el-header><h1>Eladmin仿制系统-更高级的Eladmin</h1></el-header>
+    <el-container>
+        <el-aside width="200px">侧边栏</el-aside>
+        <el-main>{{mydata}}</el-main>
+    </el-container>
+    </el-container>
+  </div>
 </template>
-
 <script>
+
+
 export default {
     name:'LoginIn',
     data(){
         return{
-            mydata:"你好啊"
+            mydata:"你好啊哈哈哈哈"
         }
     }
 }
@@ -20,8 +29,41 @@ export default {
     height: 200px;
     float: left;
     position: absolute;
-    top: 90px;
+    top: 390px;
     left: 300px;
     font-family: fangsong;
 }
+  .el-header, .el-footer {
+    background-color: #B3C0D1;
+    color: #333;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    background-color: #D3DCE6;
+    color: #333;
+    text-align: center;
+    line-height: 200px;
+  }
+  
+  .el-main {
+    background-color: #E9EEF3;
+    color: #333;
+    text-align: center;
+    line-height: 160px;
+  }
+  
+  body > .el-container {
+    margin-bottom: 40px;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
