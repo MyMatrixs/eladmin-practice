aed9f17768a1803aee7094800af10fa79319a88b
MyMatrixs <wangwanli438@gmail.com>
2023-05-20 23:12:37 +0800

    初始化leaflet地图

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import router from './router'
 import store from './store'
 
 
+
 Vue.config.productionTip = false
 Vue.use(ElementUI);
 
--- src/views/webgis/leaflet/index.vue
+++ src/views/webgis/leaflet/index.vue
@@ -1,18 +1,43 @@
 <template>
-  <div>Leaflet</div>
+  <div class="leafletMain">
+    <h1>Leaflet</h1>
+    <div id='MapContiner'></div>
+  </div>
 </template>
 
 <script>
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css';
 export default {
     name:'LeafletWebgis',
     data(){
         return{
             
         }
+    },
+    mounted(){
+        console.log("开始啦")
+        const map = L.map('MapContiner', {
+            center: [51.505, -0.09],
+            zoom: 13
+        });
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
     }
 }
 </script>
 
-<style>
 
+<style>
+.leafletMain{
+    width: 100%;
+    height: 100%;
+}
+#MapContiner{
+    width: 100%;
+    height: 90%;
+    background-color: #ffffff50;
+}
 </style>
